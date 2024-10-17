a1c84ab66815d16556f9bd173612ed5c43f28600
shenmo <jifengshenmo@outlook.com>
2024-10-17 03:01:49 +0000

    改读配置的目录
    Signed-off-by: shenmo <jifengshenmo@outlook.com>

--- gxde.sh
+++ gxde.sh
@@ -4,6 +4,6 @@ export QT_QPA_PLATFORMTHEME=deepin
 # 用于解决 deepin 截图无法使用的问题
 export DBUS_FATAL_WARNINGS=0
 # 紧凑模式设置
-if [[ -f $HOME/.config/gxde/dtk/SIZEMODE ]]; then
+if [[ -f $HOME/.config/GXDE/dtk/SIZEMODE ]]; then
 	export D_DTK_SIZEMODE=1
 fi
